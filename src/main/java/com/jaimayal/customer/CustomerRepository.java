75978bc53c8c83b98ea827ecb066de5d55978847
Jaimayal <jaime.ayalaa.76@gmail.com>
2023-04-26 13:13:39 -0600

    Code review 26/04/2023

--- src/main/java/com/jaimayal/customer/CustomerRepository.java
+++ src/main/java/com/jaimayal/customer/CustomerRepository.java
@@ -3,6 +3,7 @@ package com.jaimayal.customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
+    
     boolean existsCustomerByEmail(String email);
     boolean existsCustomerById(Long customerId);
 }
