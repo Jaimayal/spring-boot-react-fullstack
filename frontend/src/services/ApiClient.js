2c16df9897bce6dab4f2613ab2af869c1803bacc
Jaimayal <jaime.ayalaa.76@gmail.com>
2023-05-04 17:26:10 -0600

    Implemented Create and Delete functionalities

--- frontend/src/services/ApiClient.js
+++ frontend/src/services/ApiClient.js
@@ -3,3 +3,7 @@ import axios from "axios"
 export async function getCustomers() {
     return await axios.get(`http://localhost:8081/api/v1/customers`)
 }
+
+export async function saveCustomer(customer) {
+    return await axios.post(`http://localhost:8081/api/v1/customers`, customer)
+}
