e752cd01f51ff7bcd49628feae6cf7b4aae6fd52
Jaimayal <jaime.ayalaa.76@gmail.com>
2023-05-06 13:56:40 -0600

    Removed unnecessary imports

--- backend/src/main/java/com/jaimayal/config/SecurityConfig.java
+++ backend/src/main/java/com/jaimayal/config/SecurityConfig.java
@@ -1,20 +1,14 @@
 package com.jaimayal.config;
 
-import com.jaimayal.security.JwtAuthenticationFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 public class SecurityConfig {
